f224110acad77fa6c34586d58696010191a5ef2d
ronnywang <524019753@qq.com>
2022-03-03 14:21:02 +0800

    add slice_grad kernel (#10)

--- kernels/slice_kernel.cc
+++ kernels/slice_kernel.cc
@@ -46,89 +46,162 @@ void UpdateAttr(const phi::DDim& in_dims, const std::vector<int> axes,
 namespace custom_kernel {
 
 template <typename T, typename Context>
-void SliceKernel(const Context& dev_ctx,
-                            const phi::DenseTensor& x,
-                            const phi::ScalarArray& axes_array,
-                            const phi::ScalarArray& starts_array,
-                            const phi::ScalarArray& ends_array,
-                            phi::DenseTensor* out) {
+void SliceKernel(const Context& dev_ctx, const phi::DenseTensor& x,
+                 const phi::ScalarArray& axes_array,
+                 const phi::ScalarArray& starts_array,
+                 const phi::ScalarArray& ends_array, phi::DenseTensor* out) {
+  auto axes_int = axes_array.GetData();
+  auto starts_int = starts_array.GetData();
+  auto ends_int = ends_array.GetData();
+  std::vector<int> axes(axes_int.begin(), axes_int.end());
+  std::vector<int> starts(starts_int.begin(), starts_int.end());
+  std::vector<int> ends(ends_int.begin(), ends_int.end());
 
-    auto axes_int = axes_array.GetData();
-    auto starts_int = starts_array.GetData();
-    auto ends_int = ends_array.GetData();
-    std::vector<int> axes(axes_int.begin(), axes_int.end());
-    std::vector<int> starts(starts_int.begin(), starts_int.end());
-    std::vector<int> ends(ends_int.begin(), ends_int.end());
+  // auto decrease_axis = ctx.Attr<std::vector<int>>("decrease_axis");
+  // auto infer_flags = ctx.Attr<std::vector<int>>("infer_flags");
 
-    // auto decrease_axis = ctx.Attr<std::vector<int>>("decrease_axis");
-    // auto infer_flags = ctx.Attr<std::vector<int>>("infer_flags");
+  const auto& in_dims = x.dims();
 
-    const auto& in_dims = x.dims();
+  // Get the accurate attribute value of starts and ends
+  // auto starts_tensor_list = ctx.MultiInput<Tensor>("StartsTensorList");
+  // if (ctx.HasInput("StartsTensor")) {
+  //   starts = GetDataFromTensor<int>(ctx.Input<Tensor>("StartsTensor"));
+  // } else if (starts_tensor_list.size() > 0) {
+  //   starts = GetDataFromTensorList<int>(starts_tensor_list);
+  // }
 
-    // Get the accurate attribute value of starts and ends
-    // auto starts_tensor_list = ctx.MultiInput<Tensor>("StartsTensorList");
-    // if (ctx.HasInput("StartsTensor")) {
-    //   starts = GetDataFromTensor<int>(ctx.Input<Tensor>("StartsTensor"));
-    // } else if (starts_tensor_list.size() > 0) {
-    //   starts = GetDataFromTensorList<int>(starts_tensor_list);
-    // }
+  // auto ends_tensor_list = ctx.MultiInput<Tensor>("EndsTensorList");
+  // if (ctx.HasInput("EndsTensor")) {
+  //   ends = GetDataFromTensor<int>(ctx.Input<Tensor>("EndsTensor"));
+  // } else if (ends_tensor_list.size() > 0) {
+  //   ends = GetDataFromTensorList<int>(ends_tensor_list);
+  // }
 
-    // auto ends_tensor_list = ctx.MultiInput<Tensor>("EndsTensorList");
-    // if (ctx.HasInput("EndsTensor")) {
-    //   ends = GetDataFromTensor<int>(ctx.Input<Tensor>("EndsTensor"));
-    // } else if (ends_tensor_list.size() > 0) {
-    //   ends = GetDataFromTensorList<int>(ends_tensor_list);
-    // }
+  // PADDLE_ENFORCE_EQ(
+  //     starts.size(), axes.size(),
+  //     platform::errors::InvalidArgument(
+  //         "The size of starts must be equal to the size of axes."));
+  // PADDLE_ENFORCE_EQ(
+  //     ends.size(), axes.size(),
+  //     platform::errors::InvalidArgument(
+  //         "The size of ends must be equal to the size of axes."));
 
-    // PADDLE_ENFORCE_EQ(
-    //     starts.size(), axes.size(),
-    //     platform::errors::InvalidArgument(
-    //         "The size of starts must be equal to the size of axes."));
-    // PADDLE_ENFORCE_EQ(
-    //     ends.size(), axes.size(),
-    //     platform::errors::InvalidArgument(
-    //         "The size of ends must be equal to the size of axes."));
+  // if (ctx.HasInput("StartsTensor") || ctx.HasInput("EndsTensor") ||
+  //     starts_tensor_list.size() > 0 || ends_tensor_list.size() > 0) {
+  //   // Infer output dims
+  //   auto out_dims = out->dims();
+  //   auto slice_dims = out_dims;
+  //   for (size_t i = 0; i < axes.size(); ++i) {
+  //     // when start == -1 && end == start+1
+  //     if (starts[i] == -1 && ends[i] == 0 && infer_flags[i] == -1) {
+  //       auto ret =
+  //           std::find(decrease_axis.begin(), decrease_axis.end(), axes[i]);
+  //       if (ret != decrease_axis.end()) {
+  //         ends[i] = in_dims[axes[i]];
+  //       }
+  //     }
+  //   }
 
-    // if (ctx.HasInput("StartsTensor") || ctx.HasInput("EndsTensor") ||
-    //     starts_tensor_list.size() > 0 || ends_tensor_list.size() > 0) {
-    //   // Infer output dims
-    //   auto out_dims = out->dims();
-    //   auto slice_dims = out_dims;
-    //   for (size_t i = 0; i < axes.size(); ++i) {
-    //     // when start == -1 && end == start+1
-    //     if (starts[i] == -1 && ends[i] == 0 && infer_flags[i] == -1) {
-    //       auto ret =
-    //           std::find(decrease_axis.begin(), decrease_axis.end(), axes[i]);
-    //       if (ret != decrease_axis.end()) {
-    //         ends[i] = in_dims[axes[i]];
-    //       }
-    //     }
-    //   }
+  //   CheckAndUpdateSliceAttrs(in_dims, axes, &starts, &ends);
+  //   slice_dims =
+  //       GetSliceDims<int>(in_dims, axes, starts, ends, nullptr, nullptr);
+  //   out_dims = GetDecreasedDims(slice_dims, decrease_axis);
 
-    //   CheckAndUpdateSliceAttrs(in_dims, axes, &starts, &ends);
-    //   slice_dims =
-    //       GetSliceDims<int>(in_dims, axes, starts, ends, nullptr, nullptr);
-    //   out_dims = GetDecreasedDims(slice_dims, decrease_axis);
+  //   out->Resize(out_dims);
+  // }
 
-    //   out->Resize(out_dims);
-    // }
+  dev_ctx.template Alloc<T>(out);
 
-    dev_ctx.template Alloc<T>(out);
+  std::vector<int> offsets(in_dims.size());
+  std::vector<int> size(in_dims.size());
 
-    std::vector<int> offsets(in_dims.size());
-    std::vector<int> size(in_dims.size());
+  UpdateAttr(in_dims, axes, starts, ends, &offsets, &size);
 
-    UpdateAttr(in_dims, axes, starts, ends, &offsets, &size);
-
-    auto stream = static_cast<aclrtStream>(dev_ctx.stream());
-    const auto& runner = NpuOpRunner("SliceD", {x}, {*out},
-                                     {{"offsets", offsets}, {"size", size}});
-    runner.Run(stream);
+  auto stream = static_cast<aclrtStream>(dev_ctx.stream());
+  const auto& runner = NpuOpRunner("SliceD", {x}, {*out},
+                                   {{"offsets", offsets}, {"size", size}});
+  runner.Run(stream);
 }
 
-} // namespace custom_kernel
+template <typename T, typename Context>
+void SliceGradKernel(const Context& dev_ctx, const phi::DenseTensor& x,
+                     const phi::DenseTensor& out_grad,
+                     const phi::ScalarArray& axes_array,
+                     const phi::ScalarArray& starts_array,
+                     const phi::ScalarArray& ends_array,
+                     phi::DenseTensor* x_grad) {
+  auto axes_int = axes_array.GetData();
+  auto starts_int = starts_array.GetData();
+  auto ends_int = ends_array.GetData();
 
-PD_REGISTER_PLUGIN_KERNEL(slice,
-                          Ascend910,
-                          ALL_LAYOUT,
-                          custom_kernel::SliceKernel, phi::dtype::float16, float, double, int16_t, int32_t, int64_t, bool) {}
+  std::vector<int> axes(axes_int.begin(), axes_int.end());
+  std::vector<int> starts(starts_int.begin(), starts_int.end());
+  std::vector<int> ends(ends_int.begin(), ends_int.end());
+
+  // Get the accurate attribute value of starts and ends
+  // auto starts_tensor_list = ctx.MultiInput<Tensor>("StartsTensorList");
+  // if (ctx.HasInput("StartsTensor")) {
+  //   starts = GetDataFromTensor<int>(ctx.Input<Tensor>("StartsTensor"));
+  // } else if (starts_tensor_list.size() > 0) {
+  //   starts = GetDataFromTensorList<int>(starts_tensor_list);
+  // }
+
+  // auto ends_tensor_list = ctx.MultiInput<Tensor>("EndsTensorList");
+  // if (ctx.HasInput("EndsTensor")) {
+  //   ends = GetDataFromTensor<int>(ctx.Input<Tensor>("EndsTensor"));
+  // } else if (ends_tensor_list.size() > 0) {
+  //   ends = GetDataFromTensorList<int>(ends_tensor_list);
+  // }
+
+  const auto& in_dims = x.dims();
+  int rank = in_dims.size();
+
+  std::vector<int> offsets(rank);
+  std::vector<int> size(rank);
+  UpdateAttr(in_dims, axes, starts, ends, &offsets, &size);
+
+  std::vector<std::vector<int64_t>> paddings(rank, std::vector<int64_t>(2));
+  for (int i = 0; i < rank; ++i) {
+    paddings[i][0] = static_cast<int64_t>(offsets[i]);
+    paddings[i][1] = static_cast<int64_t>(in_dims[i] - size[i] - offsets[i]);
+  }
+
+  phi::DenseTensor tmp_dout;
+  tmp_dout.ShareDataWith(out_grad);
+  auto out_dims = out_grad.dims();
+  // auto decrease_axis = ctx.Attr<std::vector<int>>("decrease_axis");
+  // auto decrease_size = decrease_axis.size();
+  // if (decrease_size > 0) {
+  //   if (decrease_size == static_cast<size_t>(in_dims.size())) {
+  //     out_dims = phi::make_ddim(std::vector<int>(decrease_size, 1));
+  //   } else {
+  //     std::vector<int> origin_out_shape(out_dims.size() + decrease_size, -1);
+  //     for (size_t i = 0; i < decrease_size; ++i) {
+  //       origin_out_shape[decrease_axis[i]] = 1;
+  //     }
+  //     int index = 0;
+  //     for (size_t i = 0; i < origin_out_shape.size(); ++i) {
+  //       if (origin_out_shape[i] == -1) {
+  //         origin_out_shape[i] = out_dims[index];
+  //         ++index;
+  //       }
+  //     }
+  //     out_dims = phi::make_ddim(origin_out_shape);
+  //   }
+  //   tmp_dout.Resize(out_dims);
+  // }
+
+  dev_ctx.template Alloc<T>(x_grad);
+  auto stream = static_cast<aclrtStream>(dev_ctx.stream());
+  const auto& runner =
+      NpuOpRunner("PadD", {tmp_dout}, {*x_grad}, {{"paddings", paddings}});
+  runner.Run(stream);
+}
+}  // namespace custom_kernel
+
+PD_REGISTER_PLUGIN_KERNEL(slice, Ascend910, ALL_LAYOUT,
+                          custom_kernel::SliceKernel, phi::dtype::float16,
+                          float, double, int16_t, int32_t, int64_t, bool) {}
+PD_REGISTER_PLUGIN_KERNEL(slice_grad, Ascend910, ALL_LAYOUT,
+                          custom_kernel::SliceGradKernel, phi::dtype::float16,
+                          float, double, int16_t, int32_t, int64_t, bool) {}
--- patches/ascend_kernel.cc
+++ patches/ascend_kernel.cc
@@ -1,5 +1,7 @@
 #include "paddle/phi/kernels/ascend_kernel.h"
+#include "paddle/phi/backends/cpu/cpu_context.h"
 #include "paddle/phi/common/scalar_array.h"
+#include "paddle/phi/core/kernel_registry.h"
 
 namespace phi {
 
@@ -7,40 +9,30 @@ template <typename T, typename Context>
 void MinRawKernel(const Context& dev_ctx, const phi::DenseTensor& x,
                   const std::vector<int64_t>& axes, bool keep_dim,
                   bool reduce_all, phi::DenseTensorMeta::DataType out_dtype,
-                  phi::DenseTensor* out) {
-  out = out;
-}
+                  phi::DenseTensor* out) {}
 
 template <typename T, typename Context>
 void MinKernel(const Context& dev_ctx, const phi::DenseTensor& x,
                const std::vector<int64_t>& dims,
                phi::DenseTensorMeta::DataType out_dtype, bool keep_dim,
-               phi::DenseTensor* out) {
-  out = out;
-}
+               phi::DenseTensor* out) {}
 
 template <typename T, typename Context>
 void MaxRawKernel(const Context& dev_ctx, const phi::DenseTensor& x,
                   const std::vector<int64_t>& axes, bool keep_dim,
                   bool reduce_all, phi::DenseTensorMeta::DataType out_dtype,
-                  phi::DenseTensor* out) {
-  out = out;
-}
+                  phi::DenseTensor* out) {}
 
 template <typename T, typename Context>
 void MaxKernel(const Context& dev_ctx, const phi::DenseTensor& x,
                const std::vector<int64_t>& dims,
                phi::DenseTensorMeta::DataType out_dtype, bool keep_dim,
-               phi::DenseTensor* out) {
-  out = out;
-}
+               phi::DenseTensor* out) {}
 
 template <typename T, typename Context>
 void MeanGradKernel(const Context& dev_ctx, const phi::DenseTensor& x,
                     const std::vector<int64_t>& dim, bool keep_dim,
-                    phi::DenseTensor* out) {
-  out = out;
-}
+                    phi::DenseTensor* out) {}
 
 template <typename T, typename Context>
 void MeanRawGradKernel(const Context& dev_ctx, const phi::DenseTensor& x,
@@ -55,25 +47,27 @@ void SliceKernel(const Context& dev_ctx, const phi::DenseTensor& x,
                  const phi::ScalarArray& ends_array, phi::DenseTensor* out) {}
 
 template <typename T, typename Context>
+void SliceGradKernel(const Context& dev_ctx, const phi::DenseTensor& x,
+                     const phi::DenseTensor& out_grad,
+                     const phi::ScalarArray& axes_array,
+                     const phi::ScalarArray& starts_array,
+                     const phi::ScalarArray& ends_array,
+                     phi::DenseTensor* x_grad) {}
+
+template <typename T, typename Context>
 void SGDKernel(const Context& dev_ctx, const phi::DenseTensor& param_var,
                const phi::DenseTensor& learning_rate,
                const phi::DenseTensor& grad_var, phi::DenseTensor* param_out) {}
 
 template <typename T, typename Context>
-void ArgsortKernel(const Context& dev_ctx,
-               const phi::DenseTensor& x,
-               int axis,
-               bool descending,
-               phi::DenseTensor* output,
-               phi::DenseTensor* indices) {}
+void ArgsortKernel(const Context& dev_ctx, const phi::DenseTensor& x, int axis,
+                   bool descending, phi::DenseTensor* output,
+                   phi::DenseTensor* indices) {}
 
-#define DECALRE_COMPARE_KERNEL(compare_kernel) \
-  template <typename T, typename Context>      \
-  void compare_kernel(const Context& ctx,      \
-                      const DenseTensor& x,    \
-                      const DenseTensor& y,    \
-                      int axis,                \
-                      DenseTensor* out) {}
+#define DECALRE_COMPARE_KERNEL(compare_kernel)                  \
+  template <typename T, typename Context>                       \
+  void compare_kernel(const Context& ctx, const DenseTensor& x, \
+                      const DenseTensor& y, int axis, DenseTensor* out) {}
 
 DECALRE_COMPARE_KERNEL(LessThanKernel)
 DECALRE_COMPARE_KERNEL(GreaterEqualKernel)
@@ -83,29 +77,21 @@ DECALRE_COMPARE_KERNEL(NotEqualKernel)
 
 template <typename T, typename Context>
 void SoftmaxWithCrossEntropyKernel(const Context& dev_ctx,
-                   const phi::DenseTensor& logits,
-                   const phi::DenseTensor& labels,
-                   bool soft_label,
-                   bool use_softmax,
-                   bool numeric_stable_mode,
-                   int ignore_index,
-                   int axis,
-                   phi::DenseTensor* softmax,
-                   phi::DenseTensor* loss,
-                   phi::DenseTensor* backprop) {}
+                                   const phi::DenseTensor& logits,
+                                   const phi::DenseTensor& labels,
+                                   bool soft_label, bool use_softmax,
+                                   bool numeric_stable_mode, int ignore_index,
+                                   int axis, phi::DenseTensor* softmax,
+                                   phi::DenseTensor* loss,
+                                   phi::DenseTensor* backprop) {}
 
 template <typename T, typename Context>
-void SoftmaxWithCrossEntropyGradKernel(const Context& dev_ctx,
-                   const phi::DenseTensor& labels,
-                   const phi::DenseTensor& softmax,
-                   const phi::DenseTensor& backprop,
-                   const phi::DenseTensor& loss_grad,
-                   bool soft_label,
-                   bool use_softmax,
-                   bool numeric_stable_mode,
-                   int ignore_index,
-                   int axis,
-                   phi::DenseTensor* logits_grad) {}
+void SoftmaxWithCrossEntropyGradKernel(
+    const Context& dev_ctx, const phi::DenseTensor& labels,
+    const phi::DenseTensor& softmax, const phi::DenseTensor& backprop,
+    const phi::DenseTensor& loss_grad, bool soft_label, bool use_softmax,
+    bool numeric_stable_mode, int ignore_index, int axis,
+    phi::DenseTensor* logits_grad) {}
 
 }  // namespace phi
 
@@ -128,52 +114,25 @@ PD_REGISTER_KERNEL(mean_grad, CPU, ALL_LAYOUT, phi::MeanGradKernel, float,
 PD_REGISTER_KERNEL(slice, CPU, ALL_LAYOUT, phi::SliceKernel, float, double,
                    bool) {}
 
+PD_REGISTER_KERNEL(slice_grad, CPU, ALL_LAYOUT, phi::SliceGradKernel, float,
+                   double, bool) {}
+
 PD_REGISTER_KERNEL(sgd, CPU, ALL_LAYOUT, phi::SGDKernel, float, double, bool) {}
 
-PD_REGISTER_KERNEL(argsort, CPU, ALL_LAYOUT, phi::ArgsortKernel, float, double) {}
-
-PD_REGISTER_KERNEL(less_than,
-                   CPU,
-                   ALL_LAYOUT,
-                   phi::LessThanKernel,
-                   bool,
-                   int16_t,
-                   int,
-                   int64_t,
-                   float,
-                   double) {}
-PD_REGISTER_KERNEL(greater_equal,
-                   CPU,
-                   ALL_LAYOUT,
-                   phi::GreaterEqualKernel,
-                   bool,
-                   int16_t,
-                   int,
-                   int64_t,
-                   float,
-                   double) {}
-PD_REGISTER_KERNEL(equal,
-                   CPU,
-                   ALL_LAYOUT,
-                   phi::EqualKernel,
-                   bool,
-                   int16_t,
-                   int,
-                   int64_t,
-                   float,
-                   double) {}
-PD_REGISTER_KERNEL(not_equal,
-                   CPU,
-                   ALL_LAYOUT,
-                   phi::NotEqualKernel,
-                   bool,
-                   int16_t,
-                   int,
-                   int64_t,
-                   float,
+PD_REGISTER_KERNEL(argsort, CPU, ALL_LAYOUT, phi::ArgsortKernel, float,
                    double) {}
 
-PD_REGISTER_KERNEL(
-    softmax_with_cross_entropy, CPU, ALL_LAYOUT, phi::SoftmaxWithCrossEntropyKernel, float, double) {}
+PD_REGISTER_KERNEL(less_than, CPU, ALL_LAYOUT, phi::LessThanKernel, bool,
+                   int16_t, int, int64_t, float, double) {}
+PD_REGISTER_KERNEL(greater_equal, CPU, ALL_LAYOUT, phi::GreaterEqualKernel,
+                   bool, int16_t, int, int64_t, float, double) {}
+PD_REGISTER_KERNEL(equal, CPU, ALL_LAYOUT, phi::EqualKernel, bool, int16_t, int,
+                   int64_t, float, double) {}
+PD_REGISTER_KERNEL(not_equal, CPU, ALL_LAYOUT, phi::NotEqualKernel, bool,
+                   int16_t, int, int64_t, float, double) {}
 
-PD_REGISTER_KERNEL(softmax_with_cross_entropy_grad, CPU, ALL_LAYOUT, phi::SoftmaxWithCrossEntropyGradKernel, float) {}
+PD_REGISTER_KERNEL(softmax_with_cross_entropy, CPU, ALL_LAYOUT,
+                   phi::SoftmaxWithCrossEntropyKernel, float, double) {}
+
+PD_REGISTER_KERNEL(softmax_with_cross_entropy_grad, CPU, ALL_LAYOUT,
+                   phi::SoftmaxWithCrossEntropyGradKernel, float) {}
