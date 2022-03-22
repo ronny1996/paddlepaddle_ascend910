00f21a80b90e13cff7bd2a410be74b3789a17d75
ronnywang <524019753@qq.com>
2022-03-22 16:28:18 +0800

    update (#22)

--- patches/ascend_sig.cc
+++ patches/ascend_sig.cc
@@ -92,10 +92,19 @@ KernelSignature SoftmaxWithCrossEntropyGradOpArgumentMapping(
                          {GradVarName("Logits")});
 }
 
+KernelSignature MergedMomentOpArgumentMapping(
+    const ArgumentMappingContext& ctx) {
+  return KernelSignature(
+      "momentum", {"Param", "Grad", "Velocity", "LearningRate"},
+      {"mu", "use_nesterov", "regularization_method", "regularization_coeff",
+       "multi_precision", "rescale_grad"},
+      {"ParamOut", "VelocityOut"});
+}
 }  // namespace phi
 
 // PD_REGISTER_BASE_KERNEL_NAME(reduce_min, min);
 // PD_REGISTER_BASE_KERNEL_NAME(reduce_mean_grad, mean_grad);
+PD_REGISTER_ARG_MAPPING_FN(merged_momentum, phi::MergedMomentOpArgumentMapping);
 
 // PD_REGISTER_ARG_MAPPING_FN(reduce_min, phi::ReduceMinOpArgumentMapping);
 // PD_REGISTER_ARG_MAPPING_FN(reduce_mean_grad,
